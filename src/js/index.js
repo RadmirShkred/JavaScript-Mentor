a5686ee316713f65874cc4110409215b44d2cf62
radmir-shkred <psitecton@gmail.com>
2019-10-22 14:04:46 +0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,3 @@
 import '../scss/style.scss';
 
-var swiper = new Swiper('.swiper-container', {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  freeMode: true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-});
-
+console.log('Works!');
