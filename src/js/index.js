e1270737f5055ec5b5944ecb9ce2e1ab10084fd7
radmir-shkred <psitecton@gmail.com>
2019-10-23 14:19:44 +0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,13 @@
 import '../scss/style.scss';
+import Swiper from './swiper.min.js';
 
-console.log('Works!');
+
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 2,
+  spaceBetween: 10,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
+
